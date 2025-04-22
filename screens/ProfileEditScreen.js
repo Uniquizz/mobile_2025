cfcf46e0c5afa05c782c2db987390eafc54b236b
Isra48 <israelalbarran48@gmail.com>
2025-04-22 15:35:41 -0600

    profile edit screen styles

--- screens/ProfileEditScreen.js
+++ screens/ProfileEditScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { Camera } from 'react-native-feather';
 import PrimaryButton from '../components/PrimaryButton';
 
@@ -11,70 +11,68 @@ const ProfileEditScreen = ({
 }) => {
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0} // Ajusta este valor si el teclado aún tapa cosas
+    >
+      <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
 
+        {/* Imagen de perfil */}
+        <TouchableOpacity style={styles.avatar}>
+          <Camera color="#fff" width={24} height={24} />
+        </TouchableOpacity>
 
-      {/* Imagen de perfil */}
-      <TouchableOpacity style={styles.avatar}>
-        <Camera color="#fff" width={24} height={24} />
-      </TouchableOpacity>
+        <Text style={styles.label}>Nombre completo</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre completo"
+          placeholderTextColor="#999"
+          value={fullName}
+          onChangeText={setFullName}
+        />
 
-      <Text style={styles.label}>Nombre completo</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nombre completo"
-        placeholderTextColor="#999"
-        value={fullName}
-        onChangeText={setFullName}
-      />
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          value={userEmail}
+          onChangeText={setUserEmail}
+        />
 
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#999"
-        keyboardType="email-address"
-        value={userEmail}
-        onChangeText={setUserEmail}
-      />
+        <Text style={styles.label}>Contraseña</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          placeholderTextColor="#999"
+          secureTextEntry
+          value={userPassword}
+          onChangeText={setUserPassword}
+        />
 
-      <Text style={styles.label}>Contraseña</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Contraseña"
-        placeholderTextColor="#999"
-        secureTextEntry
-        value={userPassword}
-        onChangeText={setUserPassword}
-      />
+        <Text style={styles.label}>Celular</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Número de WhatsApp"
+          placeholderTextColor="#999"
+          keyboardType="phone-pad"
+          value={whatsappNumber}
+          onChangeText={setWhatsappNumber}
+        />
 
-      <Text style={styles.label}>WhatsApp</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Número de WhatsApp"
-        placeholderTextColor="#999"
-        keyboardType="phone-pad"
-        value={whatsappNumber}
-        onChangeText={setWhatsappNumber}
-      />
-
-   
-      <PrimaryButton
-        style={{ marginTop: 40 }}
-        label="Guardar"
-        onPress={handleSaveProfile}
-      />
-    </View>
+        <PrimaryButton style={styles.btn} label="Guardar" onPress={handleSaveProfile} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
-
-    flex: 1,
+    flexGrow: 1,
   },
-
   avatar: {
     backgroundColor: '#333',
     borderRadius: 999,
@@ -97,7 +95,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 18,
   },
-
+  btn: {
+    marginTop: 40,
+  },
 });
 
 export default ProfileEditScreen;
